cfe5b0e184427826972d1901578bff540d240a8f
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-09-20 11:08:57 +0200

    [REFACTOR]

--- cookbooks/cyber-dojo-server/recipes/default.rb
+++ cookbooks/cyber-dojo-server/recipes/default.rb
@@ -3,3 +3,8 @@ languages = %w{default-jdk libghc6-hunit-dev erlang}
 languages.each do |language|
   package language
 end
+
+package git
+
+include_recipe application_ruby
+include_recipe cyber-dojo
